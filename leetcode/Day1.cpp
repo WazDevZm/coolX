39833efa1978d9e61c7450ef729d26455cf8a646
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-09-15 13:24:06 +0200

    Added some more files to it

---
+++ leetcode/Day1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+// in this section of code, we will be learning leetcode for C++ in detail and all the good stuff!!!
+
+int main(){
+    cout<<"Hello Leetcode"<<endl;
+
+
+
+
+
+
+
+
+
+
+
+    return 8;
+}
