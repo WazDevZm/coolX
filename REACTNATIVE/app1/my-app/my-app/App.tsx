6a66abada1c8f69f0c39ead1453501cc6acf4e27
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-09-23 07:27:23 +0200

    Fix typo in App.tsx text

--- REACTNATIVE/app1/my-app/my-app/App.tsx
+++ REACTNATIVE/app1/my-app/my-app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>.tsx to start working on your app!</Text>
+      <Text>i.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
